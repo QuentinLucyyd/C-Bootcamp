c29066a4732d37fcbb650609bf0a69bff767b810
Zakhele HAKONZE <zhakonze@e5r1p3.wethinkcode.co.za>
2018-06-04 16:26:11 +0200

    Account Class Cpp Update

--- d00/ex02/Account.class.cpp
+++ d00/ex02/Account.class.cpp
@@ -6,8 +6,34 @@
 /*   By: qmanamel <marvin@42.fr>                    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2018/06/04 14:41:58 by qmanamel          #+#    #+#             */
-/*   Updated: 2018/06/04 14:42:27 by zhakonze         ###   ########.fr       */
+/*   Updated: 2018/06/04 16:23:34 by zhakonze         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
+#include "Account.class.hpp"
+#include <iostream>
 
+int	Account::_nbAccounts = 0;
+int	Account::_totalAmount = 0;
+int	Account::_totalNbDeposits = 0;
+int	Account::_totalNbWithdrawals = 0;
+
+Account::Account(int initial_deposit) {
+	this->_accountIndex = _nbAccounts;
+}
+
+int Account::getNbAccounts(void) {
+	return Account::_nbAccounts;
+}
+
+int Account::getTotalAmount(void) {
+	return Account::_totalAmount;
+}
+
+int Account::getNbDeposits(void) {
+	return Account::_totalNbDeposits;
+}
+
+int Account::getNbWithdrawals(void) {
+	return Account::_totalNbWithdrawals;
+}
